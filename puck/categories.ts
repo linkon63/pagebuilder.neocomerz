d5041a9a4cdd7a49e4f6178432c0998017470ea3
Al Mumeetu Saikat <almumeetusaikat@gmail.com>
2026-03-05 07:18:33 +0600

    refactor category name structure

--- puck/categories.ts
+++ puck/categories.ts
@@ -3,34 +3,42 @@ import { PuckProps } from "./types/puck";
 
 export const categories: Config<PuckProps>["categories"] = {
   LAYOUT: {
+    title: "LAYOUT Components",
     components: ["Container", "Columns"],
   },
 
   CONTENT: {
+    title: "CONTENT Components",
     components: ["Heading", "Text", "Button"],
   },
 
-  HERO: {
+  PREBUILT: {
+    title: "HERO Components",
     components: ["Hero", "LayeredHero", "InlineHero"],
   },
 
   FEATURES: {
+    title: "FEATURES Components",
     components: ["Features"],
   },
 
   QUALITY: {
+    title: "QUALITY Components",
     components: ["DesignAndFit"],
   },
-  
+
   GALLERY: {
+    title: "GALLERY Components",
     components: ["GalleryCol", "GalleryGrid", "GalleryGrid6"],
   },
 
   SIZING: {
+    title: "SIZING Components",
     components: ["SizeChart"],
   },
 
   COMMERCE: {
+    title: "COMMERCE Components",
     components: ["OrderForm"],
   },
 };
